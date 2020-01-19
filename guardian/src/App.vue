ab424b852bdeafda95c75c1506a8f84e06c2a23d
Denis Muhortov <48648555+MukhortovDenis@users.noreply.github.com>
2020-01-19 12:04:25 +0300

    new_function

--- guardian/src/App.vue
+++ guardian/src/App.vue
@@ -6,40 +6,37 @@
 </template>
 
 <script>
+
   import List from "@/components/List";
   export default {
     name: 'app',
     data() {
+      let url = 'https://content.guardianapis.com/search?api-key=f192c3df-0d0f-43ff-8683-930af2d73fbd';
+      let req = new Request(url);
+      fetch(req)
+              .then(response => response.json())
+              .then((data) => {
+// eslint-disable-next-line no-console
+                console.log(data);
+                let dataParse = [];
+// eslint-disable-next-line no-console
+                for (let i = 0; i < data.response.results.length; i++) {
+// eslint-disable-next-line no-console
+                  console.log("sasdasd" ,data.response.results);
+                  let obj = {
+                    title: "",
+                    url: ""
+                  };
+                  obj.title = data.response.results[i].webTitle;
+                  obj.url = data.response.results[i].webUrl;
+                  dataParse.push(obj)
+                }
+// eslint-disable-next-line no-console
+                console.log("Data is parse: ", dataParse);
+                this.obj = dataParse
+              });
       return {
-        obj: [],
-        i: this.mounted()
-      }
-    },
-    methods: {
-      mounted() {
-        let url = 'https://newsapi.org/v2/top-headlines?' +
-                'country=us&' +
-                'apiKey=ef295b674d2e48fcb3eeb97c6b3eb3c4';
-        let req = new Request(url);
-        fetch(req)
-                .then(response => response.json())
-                .then((data) => {
-                  let dataParse = [];
-                  // eslint-disable-next-line no-console
-                  for (let i = 0; i < data.articles.length; i++) {
-                    // eslint-disable-next-line no-console
-                    let obj = {
-                      title: "",
-                      url: ""
-                    };
-                    obj.title = data.articles[i].title;
-                    obj.url = data.articles[i].url;
-                    dataParse.push(obj)
-                  }
-                  // eslint-disable-next-line no-console
-                  console.log("Data is parse: ", dataParse);
-                  this.obj = dataParse
-                });
+        obj: {},
       }
     },
     components: {
